166fdaeb969e5f4401bb412d8e465086dac36b05
Nakry Mut <nakrymut375@gmail.com>
2025-04-11 13:56:21 +0700

    bugfix/: error date

--- src/app/crash-game/page.tsx
+++ src/app/crash-game/page.tsx
@@ -1,21 +1,3 @@
-// import SetupFirebaseDB from "@/components/setup-firebase-db"
-// import GameHistory from "@/components/game-history"
-
-// export default function CrashGameSetupPage() {
-//   return (
-//     <div className="container mx-auto py-8 space-y-8">
-//       <h1 className="text-3xl font-bold text-center mb-8">Crash Game Firebase Setup</h1>
-
-//       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-//         <SetupFirebaseDB />
-//         <GameHistory />
-//       </div>
-//     </div>
-//   )
-// }
-
-
-
 
 
 import MiniCrash from "@/components/crash-predictor-with-firebase"
